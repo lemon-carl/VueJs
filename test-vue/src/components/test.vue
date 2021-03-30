48e1154172ba8a1f8f7900fdecbdfaae69cbde9e
CarlLemon <LingHuan5593@qq.com>
2021-03-30 14:02:25 +0800

    单文件组件

---
+++ test-vue/src/components/test.vue
@@ -0,0 +1,28 @@
+<!--  -->
+<template>
+  <h2 class="red">test......{{ msg }}</h2>
+</template>
+
+<script>
+export default {
+  name: "test",
+  props: {
+    msg: {
+      type: String,
+      default: "hello test vue",
+    },
+  },
+  methods: {},
+  data() {
+    return {
+     
+    }
+  }
+};
+</script>
+
+<style>
+.red{
+    color: red;
+}
+</style>
