cef402e497cf90a5237e47897095f10032012b18
CarlLemon <LingHuan5593@qq.com>
2021-06-08 18:29:54 +0800

    职称功能页面

--- yeb/src/components/sys/basic/PosMana.vue
+++ yeb/src/components/sys/basic/PosMana.vue
@@ -20,34 +20,63 @@
       >
     </div>
     <div class="posManaMain">
-      <el-table size="small" stripe border :data="positions" style="width: 70%"
-                @selection-change="handleSelectionChange">
+      <el-table
+        size="small"
+        stripe
+        border
+        :data="positions"
+        style="width: 70%"
+        @selection-change="handleSelectionChange"
+      >
         <el-table-column type="selection" width="55"> </el-table-column>
         <el-table-column prop="id" label="编号" width="55"> </el-table-column>
-        <el-table-column prop="name" label="职位" width="120">
+        <el-table-column prop="name" label="职位" width="150">
         </el-table-column>
-        <el-table-column prop="createDate" label="创建时间" width="200">
+        <el-table-column prop="createDate" label="创建时间" width="150">
         </el-table-column>
         <el-table-column prop="enabled" label="是否启用" width="120">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.enabled" type="success">已启用</el-tag>
+            <el-tag v-else type="danger">未启用</el-tag>
+          </template>
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="mini" @click="showEditView(scope.$index, scope.row)">编辑</el-button>
-            <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button >
+            <el-button
+              size="mini"
+              @click="showEditView(scope.$index, scope.row)"
+              >编辑</el-button
+            >
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+              >删除</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
+       <el-button
+          size="small"
+          style="margin-top: 8px"
+          type="danger"
+          :disabled="this.multipleSelection.length == 0"
+          @click="deleteMany">批量删除</el-button>
     </div>
-    <el-button size="small" style="margin-top:8px" type="danger" 
-              :disabled="this.multipleSelection.length==0" @click="deleteMany">批量删除</el-button>
-    <el-dialog
-      title="编辑职位"
-      :visible.sync="dialogVisible"
-      width="30%"
-    >
+   
+    <el-dialog title="编辑职位" :visible.sync="dialogVisible" width="30%">
       <div>
         <el-tag>职位名称</el-tag>
         <el-input v-model="updatePos.name" class="updatePosInput"></el-input>
+         <el-tag>是否启用</el-tag>
+         <el-switch style="margin-left: 6px"
+              v-model="updatePos.enabled"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+              active-text="已启用"
+              inactive-text="未启用"
+            >
+         </el-switch>  
       </div>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
@@ -67,10 +96,11 @@ export default {
       },
       positions: [],
       dialogVisible: false,
-      updatePos:{
-        name: ''
+      updatePos: {
+        name: "",
+        enabled: false
       },
-      multipleSelection:[]
+      multipleSelection: [],
     };
   },
   mounted() {
@@ -98,17 +128,17 @@ export default {
     },
     showEditView(index, data) {
       // 数据copy
-      Object.assign(this.updatePos,data);
-      this.updatePos.createDate = '';
+      Object.assign(this.updatePos, data);
+      this.updatePos.createDate = "";
       this.dialogVisible = true;
     },
-    doUpdate(index,data){
-      this.putRequest('/system/basic/pos/', this.updatePos).then(resp=>{
-        if(resp){
+    doUpdate() {
+      this.putRequest("/system/basic/pos/", this.updatePos).then((resp) => {
+        if (resp) {
           this.initPositions();
           this.dialogVisible = false;
         }
-      })
+      });
     },
     handleDelete(index, data) {
       this.$confirm(
@@ -117,9 +147,8 @@ export default {
         {
           confirmButtonText: "确定",
           cancelButtonText: "取消",
-          type: "warning",
-        }
-      ).then(() => {
+          type: "warning"
+        }).then(() => {
           this.deleteRequest("/system/basic/pos/" + data.id).then((resp) => {
             if (resp) {
               this.initPositions();
@@ -133,34 +162,39 @@ export default {
         });
     },
     handleSelectionChange(val) {
-          this.multipleSelection = val;
-          console.log(val);
+      this.multipleSelection = val;
     },
-    deleteMany(){
-        this.$confirm("此操作将永久删除[" + this.multipleSelection.length + "]条职位, 是否继续?",
-          "提示",{
-            confirmButtonText: "确定",
-            cancelButtonText: "取消",
-            type: "warning",
-          }
-      ).then(() => {
-          let ids= '?';
-          this.multipleSelection.forEach(item =>{
-            ids+= 'ids=' + item.id + '&';
-          })
+    deleteMany() {
+      this.$confirm(
+        "此操作将永久删除[" +
+          this.multipleSelection.length +
+          "]条职位, 是否继续?",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      )
+        .then(() => {
+          let ids = "?";
+          this.multipleSelection.forEach((item) => {
+            ids += "ids=" + item.id + "&";
+          });
           this.deleteRequest("/system/basic/pos/" + ids).then((resp) => {
             if (resp) {
               this.initPositions();
             }
           });
-        }).catch(() => {
+        })
+        .catch(() => {
           this.$message({
             type: "info",
             message: "已取消删除",
           });
         });
-    }
-  }
+    },
+  },
 };
 </script>
 <style>
@@ -171,7 +205,7 @@ export default {
 .posManaMain {
   margin-top: 10px;
 }
-.updatePosInput{
+.updatePosInput {
   width: 200px;
   margin-left: 8px;
 }
